ade5420da4900178542612576cc41729a9661016
Suzanne <suzanne.aitchison@gmail.com>
2017-05-02 17:58:08 +0100

    styled and added images

--- client/src/components/SongInfo.jsx
+++ client/src/components/SongInfo.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const SongInfo = (props) => {
+  // console.log(Object.keys(props.song))
   
+  const imgLink = props.song["im:image"][0].label
+  console.log(imgLink)
   return (
     <div className="song-info">
+      <img src={imgLink} />
       <p>{props.position}. {props.song.title.label}</p>
     </div>
   )
